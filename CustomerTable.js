499a58155c2e6af423635bea541173ad1a7d8dff
kundanthota01 <141477426+kundanthota01@users.noreply.github.com>
2024-03-03 17:04:50 +0530

    Update CustomerTable.js

--- CustomerTable.js
+++ CustomerTable.js
@@ -1,38 +1,3 @@
-/*import React from 'react';
-
-const CustomerTable = ({ customers }) => {
-    return (
-        <table className="customer-table">
-            <thead>
-                <tr>
-                    <th>S.No</th>
-                    <th>Customer Name</th>
-                    <th>Age</th>
-                    <th>Phone</th>
-                    <th>Location</th>
-                    <th>Date</th>
-                    <th>Time</th>
-                </tr>
-            </thead>
-            <tbody>
-                {customers.map((customer, index) => (
-                    <tr key={index}>
-                        <td>{index + 1}</td>
-                        <td>{customer.customer_name}</td>
-                        <td>{customer.age}</td>
-                        <td>{customer.phone}</td>
-                        <td>{customer.location}</td>
-                        <td>{new Date(customer.created_at).toLocaleDateString()}</td>
-                        <td>{new Date(customer.created_at).toLocaleTimeString()}</td>
-                    </tr>
-                ))}
-            </tbody>
-        </table>
-    );
-};
-
-export default CustomerTable;
-*/
 import React from 'react';
 
 const CustomerTable = ({ customers, currentPage, customersPerPage }) => {
